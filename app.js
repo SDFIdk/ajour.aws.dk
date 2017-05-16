7c2cad27e292ce70c8dc823feea778b3e0b880a5
finnjordal <finn@jordal.dk>
2017-05-16 15:15:45 +0200

    init node

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express')
+  , rp = require('request-promise');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  console.log('get /');
+  res.sendFile(__dirname + "/public/index.html", function (err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Sent: info.html');
+    }
+  });
+});
+
+function getTicket(usr,pw) {
+  return new Promise((resolve, reject) => {
+    var options= {};
+    options.url='http://kortforsyningen.kms.dk/service';
+    options.qs= {};
+    options.qs.service= 'META';
+    options.qs.request= 'GetTicket';
+    options.qs.login= usr;
+    options.qs.password= pw;
+    //options.resolveWithFullResponse= true;
+    var jsonrequest= rp(options).then((body) => {    
+      console.log('getticket: %s, %d', body, body.length);
+      if (body.length === 32) { // returnerer en status 200 ved ukendt username/password?!
+        resolve(body);
+      }
+      else {
+        reject('Ukendt username/password');
+      }
+    })
+    .catch((err) => {
+      reject('fejl i request af kortforsyningen: ' + err);
+    });
+  });
+}
+
+app.get('/getticket', function (req, res, next) { 
+  getTicket(usr,pw).then((ticket) => {
+    res.status(200).send(ticket);
+  })
+  .catch((err) => {
+    res.status(400).send('Ukendt username og password: ' + err);
+  });
+}); 
+
+var usr= process.argv[2]
+  , pw= process.argv[3];
+
+getTicket(usr,pw).then(ticket => {
+  var server = app.listen(5000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('URL http://%s:%s', host, port);
+  });
+})
+.catch(err => {
+  console.log("Ukendt username og password (%s)",err);
+})
