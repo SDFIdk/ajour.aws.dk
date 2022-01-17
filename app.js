fc775f1000cfd4d0a81cd0d204e96dad5907f2fa
Kortforsyningen servicekonto <elvisflesborg@protonmail.com>
2022-01-17 13:43:05 +0100

    New versions of dawa-util and dawa-kort and kf-getticket. Removed comments.

--- app.js
+++ app.js
@@ -16,31 +16,6 @@ app.get('/', function (req, res) {
   });
 });
 
-// function getTicket(usr,pw) {
-//   return new Promise((resolve, reject) => {
-//     var options= {};
-//     options.url='https://api.dataforsyningen.dk/' + service;
-//     options.qs= {};
-//     options.qs.service= 'META';
-//     options.qs.request= 'GetTicket';
-//     options.qs.login= usr;
-//     options.qs.password= pw;
-//     //options.resolveWithFullResponse= true;
-//     var jsonrequest= rp(options).then((body) => {    
-//       console.log('getticket: %s, %d', body, body.length);
-//       if (body.length === 32) { // returnerer en status 200 ved ukendt username/password?!
-//         resolve(body);
-//       }
-//       else {
-//         reject('Ukendt username/password');
-//       }
-//     })
-//     .catch((err) => {
-//       reject('fejl i request af kortforsyningen: ' + err);
-//     });
-//   });
-// }
-
 app.get('/maptilerkey', function (req, res, next) { 
   res.status(200).send(maptilerkey);
 }); 
